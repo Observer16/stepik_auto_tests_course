ab029de1b83bbdc3396b1a7e6d7d229cf028ff47
Виталий Алтунин <vitalijaltunin@MacBook-Air-Vitalij.local>
2022-01-24 15:29:57 +0300

    Новые данные в test_item.py

--- test_items.py
+++ test_items.py
@@ -1,3 +1,9 @@
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
