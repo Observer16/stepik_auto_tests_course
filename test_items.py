efdbe1d1dda3f6f6203ee74695c73f50b6fde7e7
Виталий Алтунин <vitalijaltunin@MacBook-Air-Vitalij.local>
2022-01-24 17:06:53 +0300

    Новые данные в test_item.py

--- test_items.py
+++ test_items.py
@@ -1,12 +1,6 @@
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-browser = webdriver.Chrome(options=options)
-
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
 def test_button_submit(browser):
     browser.get(link)
     
