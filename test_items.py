7de0a3e29f655ebe56a6c11989c7d5272bf92b3f
Виталий Алтунин <vitalijaltunin@MacBook-Air-Vitalij.local>
2022-01-24 17:19:21 +0300

    Новые данные в test_item.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 @pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_button_submit(browser):
+def test_button_submit(self, browser, language):
     browser.get(link)
-    
+    browser.find_element_by_css_selector("#add_to_basket_form > button")
